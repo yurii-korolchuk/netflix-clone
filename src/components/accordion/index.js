d877f6a8c45099c825aa5da5112ff1c410fd23ec
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-10-30 19:36:29 +0200

    add basic components to accordion

---
+++ src/components/accordion/index.js
@@ -0,0 +1,46 @@
+import React, {createContext, useContext, useState} from "react";
+import { Container, Frame, Title, Inner, Frame, Body, Header } from './styles/accordion'
+
+const ToggleContext = createContext();
+
+export default function Accordion({children, ...restProps}) {
+  <Container {...restProps}>
+    <Inner>
+      {children}
+    </Inner>
+  </Container>
+}
+
+Accordion.Title = function AccordionTitle({children, ...restProps}) {
+  return (
+    <Title {...restProps }>{children}</Title>
+  )
+}
+
+Accordion.Frame = function AccordionFrame({children, ...restProps}) {
+  return (
+    <Frame {...restProps }>{children}</Frame>
+  )
+}
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider value={{toggleShow, setToggleShow}}>
+      <Item {...restProps }>{children}</Item >
+    </ToggleContext.Provider>
+  )
+}
+
+Accordion.Header = function AccordionHeader ({children, ...restProps}) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header {...restProps } onCLick={() => setToggleShow(toggleShow => !toggleShow)}>{children}</Header>
+  )
+}
+
+Accordion.Body = function AccordionBody({children, ...restProps}) {
+  const { toggleShow } = useContext(ToggleContext);
+
+  return toggleShow ? <Body {...restProps }>{children}</Body> : null
+}
