82bb6228393f940b740af84be376215fcdeec0ae
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-10-31 13:37:17 +0200

    add styles and finish opt-form

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -18,6 +18,7 @@ export const FaqsContainer = () => {
       <OptForm>
         <OptForm.Input placeholder='Email address' />
         <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
         <OptForm.Text>
           Ready to watch ? Enter your email to create or restart your membership.
         </OptForm.Text>
