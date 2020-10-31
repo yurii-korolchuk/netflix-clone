1187362a52caf717d84288d428c0befbaf2fd6eb
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-10-31 21:46:58 +0200

    pushed films and series data to firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import { GlobalStyles } from './global-styles'
 import 'normalize.css';
+import { firebase } from './lib/firebase.prod';
 
 ReactDOM.render(
   <>
