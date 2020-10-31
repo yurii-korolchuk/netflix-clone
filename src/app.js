c55f4b2b9bad78c821cb2983c2824f73b9ae6be8
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-10-31 17:33:33 +0200

    add Router and basic pages

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from "./containers/faqs";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as Routes from './constants/routes';
+import { Home, Browse, SignIn, SignUp }  from "./pages";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ Routes.BROWSE }>
+        <Browse />
+      </Route>
+      <Route exact path={ Routes.SIGN_IN }>
+        <SignIn />
+      </Route>
+      <Route exact path={ Routes.SIGN_UP }>
+        <SignUp />
+      </Route>
+      <Route exact path={ Routes.HOME }>
+        <Home />
+      </Route>
+    </Router>
   );
 }
