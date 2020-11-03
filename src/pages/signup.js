92384e47295f3e8daea0293312fbeb8a90b1033b
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-11-03 14:50:33 +0200

    add firebase auth

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -20,7 +20,24 @@ export default function SignUp() {
   const handleSignUp = (event) => {
     event.preventDefault()
 
-    // firebase
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then(result => {
+        result.user.updateProfile({
+          displayName: firstName,
+          photoURL: Math.floor(Math.random() * 5) + 1
+        })
+        .then(() => {
+          history.push(ROUTES.BROWSE)
+        })
+      })
+      .catch(error => {
+        setFirstName('')
+        setEmailAddress('')
+        setPassword('')
+        setError(error.message)
+      })
   }
 
 
