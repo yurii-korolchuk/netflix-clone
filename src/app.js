313706cd185b7b63abffc41a71c2ce3cb4259aeb
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-11-06 23:39:56 +0200

    add listener to user auth

--- src/app.js
+++ src/app.js
@@ -3,22 +3,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as Routes from './constants/routes';
 import { Home, Browse, SignIn, SignUp }  from "./pages";
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const temp = null
+  const { user } = useAuthListener()
 
   return (
     <Router>
-      <ProtectedRoute user={ temp } path={ Routes.BROWSE }>
+      <ProtectedRoute user={ user } path={ Routes.BROWSE }>
         <Browse />
       </ProtectedRoute>
-      <IsUserRedirect user={ temp } loggedInPath={ Routes.BROWSE } path={ Routes.SIGN_IN }>
+      <IsUserRedirect user={ user } loggedInPath={ Routes.BROWSE } path={ Routes.SIGN_IN }>
         <SignIn />
       </IsUserRedirect>
-      <IsUserRedirect user={ temp } loggedInPath={ Routes.BROWSE } path={ Routes.SIGN_UP }>
+      <IsUserRedirect user={ user } loggedInPath={ Routes.BROWSE } path={ Routes.SIGN_UP }>
         <SignUp />
       </IsUserRedirect>
-      <IsUserRedirect user={ temp } path={ Routes.HOME } loggedInPath={ Routes.BROWSE } exact>
+      <IsUserRedirect user={ user } path={ Routes.HOME } loggedInPath={ Routes.BROWSE } exact>
         <Home />
       </IsUserRedirect>
     </Router>
