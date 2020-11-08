34402abf5a8201d40db981bb7049ea8023db598c
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-11-09 00:47:38 +0200

    add styles to profiles

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import SelectProfileContainer from './profiles';
 import { FirebaseContext } from '../context/firebase';
 
 export default function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfileContainer user={user}/>
+  useEffect(() => {
+    console.log(profile)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [profile.displayName])
+
+  return <SelectProfileContainer user={user} setProfile={setProfile}/>
 }
